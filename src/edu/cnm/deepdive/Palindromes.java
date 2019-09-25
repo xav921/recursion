5d69e03e7224dbfa83de8a651c222e5d7d2b2d6d
xav921 <xav7470@gmail.com>
2019-09-25 11:59:51 -0600

    Added non-strict test.

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -3,10 +3,14 @@ package edu.cnm.deepdive;
 public class Palindromes {
 
   public static boolean isPalindrome(String input) {
+    return isStrictPalindrome(input.replaceAll("[\\W_]", "").toLowerCase());
+  }
+
+  public static boolean isStrictPalindrome(String input) {
     return input.length() <= 1
         || (
         input.charAt(0) == input.charAt(input.length() - 1)
-            && isPalindrome(input.substring(1, input.length() - 1))
+            && isStrictPalindrome(input.substring(1, input.length() - 1))
     );
   }
 
