b72ea721ceca2e4b08f5eb4d27ce4047f890548f
xav921 <xav7470@gmail.com>
2019-09-25 11:31:15 -0600

    Iterative implementation

--- src/edu/cnm/deepdive/Factorial.java
+++ src/edu/cnm/deepdive/Factorial.java
@@ -5,16 +5,15 @@ import java.math.BigInteger;
 public class Factorial {
 
   public static void main(String[] args) {
-    System.out.println(factorial(21));
+    System.out.printf("%,d%n", factorial(100000));
   }
 
   public static BigInteger factorial(int n) {
-    return (n <= 1) ? BigInteger.ONE : factorial(n - 1).multiply(BigInteger.valueOf(n));
-//    long product = 1;
-//    if (n > 1) {
-//      product = n * factorial(n - 1);
-//    }
-//    return product;
+    BigInteger product = BigInteger.ONE;
+    for (int i = 2; i <= n; i++) {
+      product = product.multiply(BigInteger.valueOf(i));
+    }
+    return product;
   }
 
 }
